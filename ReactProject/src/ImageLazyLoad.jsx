92ed21f26985132fc5924dc45f76bd4c82aec806
Rahul Sharma <111230814+rahulavashist@users.noreply.github.com>
2023-07-19 21:46:19 +0530

    Update ImageLazyLoad.jsx
    new comment add

--- ReactProject/src/ImageLazyLoad.jsx
+++ ReactProject/src/ImageLazyLoad.jsx
@@ -45,28 +45,30 @@ const App = () => {
   const containerRef = useRef();
 
   useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
+    const fetchData = async () => { // This line defines an asynchronous arrow function named fetchData, which will be used to fetch data from the API
+      setLoading(true);//Before starting the data fetching process, setLoading is called with true
       try {
-        const response = await axios.get(`/api/image/pages/${page}`);
-        const responseData = response.data;
-        if (responseData.images.length > 0) {
-          setData((prevData) => [...prevData, ...responseData.images]);
-          setLoading(false);
-          setPage((prevPage) => prevPage + 1);
+        const response = await axios.get(`/api/image/pages/${page}`);// This line sends a Get request to the API and using axios library to fetching the data (`/api/image/pages/) this is api url and (${page}) is parameter
+        const responseData = response.data;//Once the response is received, and then  extract the data  and stored in the responseData api response in json format
+        if (responseData.images.length > 0) { // This line checks if the responseData contains images .if images then it will run
+          setData((prevData) => [...prevData, ...responseData.images]);// setData function is called to update the data state.It uses the updater function to add the new images from the responseData to the existing data array. The spread operator ... is used to spread the existing prevData array and the new responseData.images array into a new array
+          setLoading(false);// After updating the data state, setLoading is called with false
+          setPage((prevPage) => prevPage + 1);// This line updates the page state using the setPage function. It takes the current value of the page state (prevPage) and increments page by 1
         } else {
-          setLoading(false);
+          setLoading(false);// If the API response does not contain any images or responseData is empty
         }
       } catch (error) {
-        console.error('Error fetching data:', error);
-        setLoading(false);
+        console.error('Error fetching data:', error); //  If an error during the API call or missing anything when api call
+        setLoading(false);// After logging the error, setLoading is called with false
       }
     };
 
-    const handleScroll = () => {
+    const handleScroll = () => {// This line defines the handleScroll function, which will be called whenever the user scrolls on the page
       if (
-        containerRef.current &&
-        window.innerHeight + window.pageYOffset >= containerRef.current.offsetHeight - 100 &&
+        containerRef.current && //This checks if the containerRef has been attached to a DOM element and also measure the scrolling position
+        window.innerHeight + window.pageYOffset >= // it checks the current vertical scroll position of the window by adding the viewport height and also check 
+the number of pixels the page has been scrolled vertically 
+ containerRef.current.offsetHeight - 100 &&
         !loading
       ) {
         fetchData();

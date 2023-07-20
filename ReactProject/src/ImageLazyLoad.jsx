9c6f6e5009972a72fb195163403cc98ee7c1d96b
Rahul Sharma <111230814+rahulavashist@users.noreply.github.com>
2023-07-20 16:58:58 +0530

    new code with comment

--- ReactProject/src/ImageLazyLoad.jsx
+++ ReactProject/src/ImageLazyLoad.jsx
@@ -1,7 +1,4 @@
 
-
-
-
 import React, { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
 import { css } from '@emotion/react';
@@ -32,9 +29,9 @@ const ImageLazyLoad = ({ src, alt }) => { // functional component named ImageLaz
     }
 
     return () => {
-      if (imageRef.current) {
-        observer.unobserve(imageRef.current);
-      }
+      // if (imageRef.current) {
+      //   observer.unobserve(imageRef.current);
+      // }
     };
   }, [src]);// The useEffect hook dependency array
 
@@ -45,7 +42,7 @@ const App = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [page, setPage] = useState(1);
-  const containerRef = useRef();
+  const containerRef = useRef(); // its work like as a container
 
   useEffect(() => {
     const fetchData = async () => { // This line defines an asynchronous arrow function named fetchData, which will be used to fetch data from the API
@@ -53,7 +50,7 @@ const App = () => {
       try {
         const response = await axios.get(`/api/image/pages/${page}`);// This line sends a Get request to the API and using axios library to fetching the data (`/api/image/pages/) this is api url and (${page}) is parameter
         const responseData = response.data;//Once the response is received, and then  extract the data  and stored in the responseData api response in json format
-        if (responseData.images.length > 0) { // This line checks if the responseData contains images .if images then it will run
+        if (responseData.images.length ) { // This line checks if the responseData contains images .if images then it will run
           setData((prevData) => [...prevData, ...responseData.images]);// setData function is called to update the data state.It uses the updater function to add the new images from the responseData to the existing data array. The spread operator ... is used to spread the existing prevData array and the new responseData.images array into a new array
           setLoading(false);// After updating the data state, setLoading is called with false
           setPage((prevPage) => prevPage + 1);// This line updates the page state using the setPage function. It takes the current value of the page state (prevPage) and increments page by 1
@@ -68,9 +65,7 @@ const App = () => {
 
     const handleScroll = () => {// This line defines the handleScroll function, which will be called whenever the user scrolls on the page
       if (
-        containerRef.current && //This checks if the containerRef has been attached to a DOM element and also measure the scrolling position
-        window.innerHeight + window.pageYOffset >= // it checks the current vertical scroll position of the window by adding the viewport height and also check the number of pixels the page has been scrolled vertically 
- containerRef.current.offsetHeight - 100 && //This condition checks if the user has scrolled close to the bottom of the container. It compares the sum of the viewport height and vertical scroll position to the container's height
+ containerRef.current.offsetHeight  && //This condition checks if the user has scrolled  to the bottom of the container. 
         !loading  //This checks if the loading state is false or no data fetched
       ) {
         fetchData(); // If the conditions within the if statement are valid then fetchData function called and this is a way to fetch new page images
@@ -79,7 +74,7 @@ const App = () => {
 
     fetchData(); // This line immediately calls the fetchData function when the component is mounted. The purpose is to load the initial set of images from the API when the component is first rendered.
 
-    window.addEventListener('scroll', handleScroll);// This line adds an event listener to the scroll event of the window. It attaches the handleScroll function to the scroll event, which means that handleScroll will be called whenever the user scrolls on the page.
+    window.addEventListener('scroll', handleScroll);// This line adds an event listener to the scroll event of the window. It attaches the handleScroll function to the scroll event. then the handleScroll will be called whenever the user scrolls on the page.
 
     return () => {
       window.removeEventListener('scroll', handleScroll);// This return statement defines a cleanup function that will be executed when the component is unmounted 
@@ -96,8 +91,8 @@ const App = () => {
     }
   };
 
-  const numColumns = getNumColumns();
-  const columnWidth = 100 / numColumns + '%';
+  const numColumns = getNumColumns(); // getNumColumns function called but stored in a numColumns variables
+  const columnWidth = 100 / numColumns  ;
 
   const override = css`
     display: block;
@@ -105,19 +100,23 @@ const App = () => {
   `;
 
   return (
-    <div ref={containerRef}> // containerRef attached to it.and  The reference will be used to track this container element
-      <div className="gallery-container" style={{ columnCount: numColumns }}> // columnCount to numColumns, which dynamically controls the number of columns based on the window width
+    <div >
+    <h1> Images</h1>
+    <hr/>
+    <div ref={containerRef}> {/* containerRef attached to it.and  The reference will be used to track this container element */}
+      <div  style={{ columnCount: numColumns }}> {/* columnCount to numColumns, which controls the number of columns based on the window width*/}
         {data.map((item, index) => (
-          <div key={index} className="gallery-item" style={{ width: columnWidth }}>
+          <div key={index}  style={{ width: columnWidth }}>
             <ImageLazyLoad src={item.image_path}  />
           </div>
         ))}
       </div>
       {loading && (
         <div className="spinner">
-          <RingLoader color="#000" css={override} size={50} />
+          <RingLoader color="#00000048" css={override} size={50} />
         </div>
       )}
+    </div>
     </div>
   );
 };
